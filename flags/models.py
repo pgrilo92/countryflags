9cc8d91922de371f82b975f8e066243357323f63
Joaquim <pgrilo92@hotmail.com>
2020-01-20 14:22:50 -0500

    first commit

---
+++ flags/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Flag(models.Model):
+        country = models.CharField(max_length=100)
+        capital_city = models.CharField(max_length=100)
+        flag_img = models.URLField()
+        continent = models.CharField(max_length=100)
+        population = models.IntegerField()
+        description = models.TextField(max_length=300)
